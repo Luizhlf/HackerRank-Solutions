4575d5e44f6f68b0cea98f298d310775254a5b6b
Luizhlf <luiz301202lh@gmail.com>
2025-08-28 20:54:34 -0300

    desafios

--- HackerRanker/catAndMouse.js
+++ HackerRanker/catAndMouse.js
@@ -29,7 +29,7 @@ function readLine() {
 function catAndMouse(x, y, z) {
 
 let catA = Math.abs(x - z);
-let catB = Math.abs(y - z);
+let catB = Math.abs(y - z)
 
 if (catA < catB) return 'Cat A';
     else if (catB < catA) return 'Cat B';
